105eee701487bbfd0671c0d848beb0e7544111a4
javaswapdevelopment <slayerhyip@protonmail.com>
2021-09-05 06:44:51 -0500

    Add address and chain id correctly

--- subgraphs/pairs/mappings/factory.ts
+++ subgraphs/pairs/mappings/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../generated/Factory/Factory";
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "./utils/erc20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0x5E90De5C6Ef6542b29B1155669C8348f82174205";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
--- subgraphs/smartchef/mappings/utils/index.ts
+++ subgraphs/smartchef/mappings/utils/index.ts
@@ -5,7 +5,7 @@ export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
 
 export let BLACKLISTED_ADDRESSES: string[] = [
-  "0x2ba38552ee50c3450427c4afe72f8dd845205a33", // HachiMoon
+  
 ];
 
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {

e1a7a1dc291245fef1b8b2ad6292d8ff44ddebed
javaswapdevelopment <javaswap.development@gmail.com>
2022-03-03 23:23:55 -0500

    Add address lowercase for usdt

--- subgraphs/exchange/mappings/pricing.ts
+++ subgraphs/exchange/mappings/pricing.ts
@@ -10,7 +10,7 @@ let USDC = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
 let WBTC = "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6";
 let WETH = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
 let JAVA = "0x4afae971ac146d4028c3ed581eb307a1615e59fe";
-let USDT = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+let USDT = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
 
 export function getMaticPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
